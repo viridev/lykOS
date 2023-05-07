ea5a35f48ad6982cb0b7e30b53c38f65935324c3
Matei <matei.lupu1104@gmail.com>
2023-05-07 16:35:36 +0300

    VS Code on Windows likes __asm__

--- kernel/src/core/io.c
+++ kernel/src/core/io.c
@@ -2,7 +2,7 @@
 
 void outb(uint16_t port, uint8_t val)
 {
-    asm volatile("outb %0, %1"
+    __asm__ volatile("outb %0, %1"
                  :
                  : "a"(val), "Nd"(port));
 }
@@ -10,7 +10,7 @@ void outb(uint16_t port, uint8_t val)
 uint8_t inb(uint16_t port)
 {
     uint8_t ret;
-    asm volatile("inb %1, %0"
+    __asm__ volatile("inb %1, %0"
                  : "=a"(ret)
                  : "Nd"(port));
     return ret;
@@ -18,30 +18,30 @@ uint8_t inb(uint16_t port)
 
 void outl(uint16_t port, uint32_t val)
 {
-    asm volatile("outl %0, %w1" : : "a" (val), "Nd" (port));
+    __asm__ volatile("outl %0, %w1" : : "a" (val), "Nd" (port));
 }
 
 uint32_t inl(uint16_t port)
 {
     uint32_t data;
-    asm volatile("inl %w1, %0" : "=a" (data) : "Nd" (port));
+    __asm__ volatile("inl %w1, %0" : "=a" (data) : "Nd" (port));
     return data;
 }
 
 void outw(uint16_t port, uint16_t val)
 {
-    asm volatile("outw %w0, %w1" : : "a" (val), "Nd" (port));
+    __asm__ volatile("outw %w0, %w1" : : "a" (val), "Nd" (port));
 }
 
 
 uint16_t inw(uint16_t port)
 {
     uint16_t data;
-    asm volatile("inw %w1, %w0" : "=a" (data) : "Nd" (port));
+    __asm__ volatile("inw %w1, %w0" : "=a" (data) : "Nd" (port));
     return data;
 }
 
 void io_wait(void)
 {
-    asm volatile ("outb %%al, $0x80" : : "a"(0));
+    __asm__ volatile ("outb %%al, $0x80" : : "a"(0));
 }
